9f781fb040023ef89b750c2c75b8f14d3f61d9f3
han16935 <han1693516@gmail.com>
2025-12-18 20:06:04 +0900

    fix : @Param() 통한 InvalidDataAccessApiUsageException 해결

--- src/main/java/com/playus/twpservice/domain/party/repository/write/PartyAgeRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/write/PartyAgeRepository.java
@@ -4,6 +4,7 @@ import com.playus.twpservice.domain.party.entity.PartyAge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface PartyAgeRepository extends JpaRepository<PartyAge, Long> {
 
     @Modifying
     @Query("UPDATE PartyAge p SET p.deletedAt = CURRENT_TIMESTAMP WHERE p.party.id = :partyId")
-    void deleteByPartyId(Long partyId);
+    void deleteByPartyId(@Param("partyId") Long partyId);
 
     List<PartyAge> findByPartyId(Long partyId);
 }
--- src/main/java/com/playus/twpservice/domain/party/repository/write/PartyJoinRepository.java
+++ src/main/java/com/playus/twpservice/domain/party/repository/write/PartyJoinRepository.java
@@ -4,6 +4,7 @@ import com.playus.twpservice.domain.party.entity.PartyJoin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,8 +12,8 @@ public interface PartyJoinRepository extends JpaRepository<PartyJoin, Long> {
 
     @Modifying
     @Query("UPDATE PartyJoin p SET p.deletedAt = CURRENT_TIMESTAMP WHERE p.party.id = :partyId")
-    void deleteByPartyId(Long partyId);
+    void deleteByPartyId(@Param("partyId") Long partyId);
 
     @Query("SELECT p FROM PartyJoin p WHERE p.party.id = :partyId AND p.userId = :userId")
-    Optional<PartyJoin> findByPartyIdAndUserId(Long partyId, Long userId);
+    Optional<PartyJoin> findByPartyIdAndUserId(@Param("partyId") Long partyId, @Param("userId") Long userId);
 }
