14baf1ee635ed799900d9064048cd6f9990e9483
han16935 <han1693516@gmail.com>
2025-12-23 19:02:18 +0900

    refactor : 락 획득 실패 시 LockAcquireFailException throw되도록 추가

--- src/main/java/com/playus/twpservice/domain/party/facade/PartyApplyFacade.java
+++ src/main/java/com/playus/twpservice/domain/party/facade/PartyApplyFacade.java
@@ -2,13 +2,17 @@ package com.playus.twpservice.domain.party.facade;
 
 import com.playus.twpservice.domain.common.security.CustomOAuth2User;
 import com.playus.twpservice.domain.party.service.PartyService;
+import com.playus.twpservice.global.exception.LockAcquireFailException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PartyApplyFacade {
@@ -20,9 +24,13 @@ public class PartyApplyFacade {
         RLock lock = redissonClient.getLock(partyId.toString());
         boolean isLocked = false;
         try {
-            isLocked = lock.tryLock(10, TimeUnit.SECONDS);
+            isLocked = lock.tryLock(0, TimeUnit.MILLISECONDS);
             if (isLocked) {
                 partyService.applyPartyFCFS(oauth2User, partyId);
+            }
+
+            else {
+                throw new LockAcquireFailException("서버가 혼잡합니다. 잠시 후 시도해주세요!");
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
