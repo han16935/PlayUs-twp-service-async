37bb1b85f9dc248ea8e068059fb0d36ceaf5b15b
han16935 <han1693516@gmail.com>
2025-12-24 18:42:27 +0900

    comment : 사용하지 않는 코드 주석 제거

--- src/main/java/com/playus/twpservice/domain/party/listener/PartyNotificationEventListener.java
+++ src/main/java/com/playus/twpservice/domain/party/listener/PartyNotificationEventListener.java
@@ -1,8 +1,9 @@
 package com.playus.twpservice.domain.party.listener;
 
+import com.playus.twpservice.domain.common.feign.client.NotificationFeignClient;
 import com.playus.twpservice.domain.common.feign.event.PartyNotificationEvent;
-import com.playus.twpservice.global.webclient.NotificationServiceClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -11,13 +12,11 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class PartyNotificationEventListener {
 
-    //    private final NotificationFeignClient notificationFeignClient;
-    private final NotificationServiceClient notificationWebClient;
+    private final NotificationFeignClient notificationFeignClient;
 
-//    @Async("notificationAsyncExecutor")
+    @Async("notificationAsyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle (PartyNotificationEvent event) {
-        notificationWebClient.notifyParty(event);
-//        notificationFeignClient.notifyParty(event);
+        notificationFeignClient.notifyParty(event);
     }
 }
